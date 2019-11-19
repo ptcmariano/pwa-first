d26f32359e815acd9c0290c591c96e8fd518dbe2
Paulo Tiago <paulotiagocm@hotmail.com>
2019-11-19 20:24:14 +0000

    complete pwa

--- server.js
+++ server.js
@@ -165,7 +165,7 @@ function startServer() {
   const app = express();
 
   // Redirect HTTP to HTTPS,
-  app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301));
+  //app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301));
 
   // Logging for each request
   app.use((req, resp, next) => {
